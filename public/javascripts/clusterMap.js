536f7920f33680c6657c6e7a848224c5bdf06f1a
Sarjeet Singh <104495979+sarjeetsingh-tech@users.noreply.github.com>
2023-07-30 17:47:33 +0530

    Update clusterMap.js
    ..

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -4,7 +4,7 @@ const map = new mapboxgl.Map({
 container: 'map',
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 style: 'mapbox://styles/mapbox/light-v11',
-center: [-103.5917, 40.6699],
+center: [78.9629,20.5937],
 zoom: 3
 });
 map.addControl(new mapboxgl.NavigationControl());
